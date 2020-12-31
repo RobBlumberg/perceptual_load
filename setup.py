e771fcc3993762a8a4f55c1ad5131868dd06064d
Rob Blumberg <55078244+RobBlumberg@users.noreply.github.com>
2020-12-31 14:22:05 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="perceptual-load", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Rob Blumberg",
     description="Perceptual Load Experiment",
     packages=setuptools.find_packages(),
