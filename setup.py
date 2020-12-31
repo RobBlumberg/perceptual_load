a86ea1627fa2b013a6189df659131ec7f1abda77
Rob Blumberg <55078244+RobBlumberg@users.noreply.github.com>
2020-12-31 14:18:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="perceptual-load", # Replace with your own username
+    version="0.0.2",
+    author="Rob Blumberg",
+    description="Perceptual Load Experiment",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
