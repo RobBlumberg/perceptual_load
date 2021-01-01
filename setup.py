7add6365453bba4bd6d86f4a21be8ca6cb9a60bc
RobBlumberg <robertblumberg97@gmail.com>
2021-01-01 13:41:32 -0500

    syntax typo and version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="perceptual-load", # Replace with your own username
-    version="0.0.6",
+    version="0.0.7",
     author="Rob Blumberg",
     description="Perceptual Load Experiment",
     packages=setuptools.find_packages(),
@@ -12,6 +12,6 @@ setup(
         'numpy>=1.18.2', 
         'pandas>=1.0.3', 
         'psychopy>=2020.2.10'
-    ]
+    ],
     python_requires='>=3.7',
 )
